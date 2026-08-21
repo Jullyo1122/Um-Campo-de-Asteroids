1e34033014645c353e1db465b36e3523c24bbc66
Jullyo <jullyoc74@gmail.com>
2026-08-21 20:08:13 -0300

    animação de explosão adicionada a nave

--- Entities/asteroids.gd
+++ Entities/asteroids.gd
@@ -10,6 +10,5 @@ func _process(delta):
 
 
 func _on_body_entered(body: Node2D) -> void:
-
 	if body.name == "Player":
-		print("A nave foi atingida!")
+		body.die()
--- Entities/player.gd
+++ Entities/player.gd
@@ -3,26 +3,42 @@ extends CharacterBody2D
 @export var SPEED: float = 300.0
 
 @onready var sprite = $Sprite2D
+@onready var explosion = $Explosion
+@onready var collision_shape = $CollisionShape2D
+
+var dead = false
 
 func _physics_process(delta):
+	if dead:
+		return
+
 	var direction = Input.get_axis("ui_left", "ui_right")
-	
+
 	velocity.x = direction * SPEED
 	velocity.y = 0
-	
+
 	move_and_slide()
-	
+
 	var screen_size = get_viewport_rect().size
 	var half_width = sprite.texture.get_width() * sprite.scale.x / 2
-	
+
 	position.x = clamp(
 		position.x,
 		half_width,
 		screen_size.x - half_width
 	)
-	
-	for collision in get_slide_collision_count():
-		var collision_info = get_slide_collision(collision)
-		var collider = collision_info.get_collider()
-		if collider.is_in_group("asteroids"):
-			print("A nave foi atingida!")
+
+
+func die():
+	if dead:
+		return
+
+	dead = true
+
+	velocity = Vector2.ZERO
+
+	sprite.hide()
+	collision_shape.set_deferred("disabled", true)
+
+	explosion.show()
+	explosion.play("default")
